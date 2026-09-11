caae248867ff26bafc1d9bb83c6adccfd8e02219
Estudiante uib <estudiante@uib.com.co>
2026-09-11 08:57:12 -0500

    implementa el menu principal para la gestion de contactos

--- Diccionarios.py
+++ Diccionarios.py
@@ -2,3 +2,28 @@ nombre = []
 telefono = []
 email = []
 direccion = []
+
+
+while True:
+    menu = """
+
+    ### ELIGE UNA OPCION ###
+    1. Agregar un contacto.
+    2. Busca un contacto.
+    3. Eliminar un contacto.
+    4. Salir
+    """
+
+    opcion_elegida = int(input(menu))
+
+    if opcion_elegida == 1:
+        pass
+    elif opcion_elegida == 2:
+        pass
+    elif opcion_elegida == 3:
+        pass
+    elif opcion_elegida == 4:
+        print("Hasta aquí llegó el programa")
+        break
+    else:
+        print("Opción invalida, intente de nuevo")
